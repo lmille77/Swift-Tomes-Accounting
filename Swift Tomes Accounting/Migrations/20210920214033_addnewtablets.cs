ef300a031760599676e7f98b86d72fc525da08d5
patrick <adjegba3414@gmail.com>
2021-09-20 18:35:57 -0400

    Rebuilding missed work

---
+++ Swift Tomes Accounting/Migrations/20210920214033_addnewtablets.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Swift_Tomes_Accounting.Migrations
+{
+    public partial class addnewtablets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
