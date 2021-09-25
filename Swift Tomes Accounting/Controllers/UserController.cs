b9cc2428db6cdd1d95a2ca52bffffc0c5d08721a
Logan Miller <54650465+lmille77@users.noreply.github.com>
2021-09-25 02:09:12 -0400

    Combined unapprove and user list, added icons to user list. Admin has to assign role before user can be approved functionality added.

--- Swift Tomes Accounting/Controllers/UserController.cs
+++ Swift Tomes Accounting/Controllers/UserController.cs
@@ -8,19 +8,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Swift_Tomes_Accounting;
+using Swift_Tomes_Accounting.Helpers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace NewSwift.Controllers
 {
     public class UserController : Controller
     {
-        private readonly UserManager<ApplicationUser> _userManager;
         private readonly ApplicationDbContext _db;
-        
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private IConfiguration _configuration;
+        private IWebHostEnvironment _webHostEnvironment;
 
-        public UserController(ApplicationDbContext db, UserManager<ApplicationUser> userManager)
+
+        public UserController(ApplicationDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
         {
-            _userManager = userManager;
             _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+            _webHostEnvironment = webHostEnvironment;
         }
 
 
@@ -158,7 +167,45 @@ namespace NewSwift.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Approve(string userId)
+        {
+            var objFromdb = _db.ApplicationUser.FirstOrDefault(u => u.Id == userId);
+            var userRole = _db.UserRoles.FirstOrDefault(u => u.UserId == objFromdb.Id);
+            var roleList = _roleManager.Roles.ToList();
+            var unapprovedID = "";
+            foreach(var role in roleList)
+            {
+                if(role.Name == "Unapproved")
+                {
+                    unapprovedID = role.Id;
+                }
+            }
+            if (objFromdb == null)
+            {
+                return NotFound();
+            }
 
+            if (objFromdb.isApproved == false && userRole.RoleId != unapprovedID)
+            {
+                //sends an email to admin requesting approval for new user
+                var email = objFromdb.Email;
+                var subject = "Accepted";
+                var body = "<a href='https://localhost:44316/Account/Login'>Click here to sign in </a>";
+                var mailHelper = new MailHelper(_configuration);
+                mailHelper.Send(_configuration["Gmail:Username"], email, subject, body);
+                objFromdb.isApproved = true;
+                TempData[SD.Success] = "User approved successfully.";
+            }
+            else
+            {
+                
+            }
+
+
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
     }
 
 }
--- Swift Tomes Accounting/Migrations/20210925055855_initial.Designer.cs
+++ Swift Tomes Accounting/Migrations/20210925055855_initial.Designer.cs
@@ -10,8 +10,8 @@ using Swift_Tomes_Accounting.Data;
 namespace Swift_Tomes_Accounting.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20210924135027_initialize")]
-    partial class initialize
+    [Migration("20210925055855_initial")]
+    partial class initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
