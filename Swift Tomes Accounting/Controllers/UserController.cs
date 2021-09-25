bfa87de814702260306c527c873e9c3c3cf99ae5
Logan Miller <54650465+lmille77@users.noreply.github.com>
2021-09-25 11:31:42 -0400

    Added assignroles view.

--- Swift Tomes Accounting/Controllers/UserController.cs
+++ Swift Tomes Accounting/Controllers/UserController.cs
@@ -84,6 +84,32 @@ namespace NewSwift.Controllers
             return View(objFromDb);
         }
 
+        public IActionResult AssignRole(string userId)
+        {
+            var objFromDb = _db.ApplicationUser.FirstOrDefault(u => u.Id == userId);
+            if (objFromDb == null)
+            {
+                return NotFound();
+            }
+
+
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+
+            //this will find if there are any roles assigned to the user
+            var role = userRole.FirstOrDefault(u => u.UserId == objFromDb.Id);
+            if (role != null)
+            {
+                objFromDb.RoleId = roles.FirstOrDefault(u => u.Id == role.RoleId).Id;
+            }
+            objFromDb.RoleList = _db.Roles.Select(u => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id
+            });
+
+            return View(objFromDb);
+        }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
