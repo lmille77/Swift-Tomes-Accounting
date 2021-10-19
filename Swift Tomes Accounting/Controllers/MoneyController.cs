d58af7ecd7a255bdc14423589554970b9d8e3b41
Logan Miller <54650465+lmille77@users.noreply.github.com>
2021-10-19 00:19:22 -0400

    Added individual account event logs

--- Swift Tomes Accounting/Controllers/MoneyController.cs
+++ Swift Tomes Accounting/Controllers/MoneyController.cs
@@ -459,6 +459,26 @@ namespace Swift_Tomes_Accounting.Controllers
             return View(objFromdb);
         }
 
+        [HttpGet]
+
+        public IActionResult AcctEventLog(int? id)
+        {
+            
+            var all_acctevents = _db.EventAccount.ToList();
+            List<EventAccount> select_account = new List<EventAccount>();
+            foreach(var acct in all_acctevents)
+            {
+                if(acct.AfterAccountNumber == id)
+                {
+                    select_account.Add(acct);
+                }
+            }
+            EventModel eventlist = new EventModel
+            {
+                EventAccount = select_account
+            };
+            return View(eventlist);
+        }
 
 
         [HttpGet]
